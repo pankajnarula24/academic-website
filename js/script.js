5efd4924da29cc1be16c4c3c825d7d76f68e85c0
pankajnarula24 <pankajnarula84@gmail.com>
2025-12-01 21:16:07 +0530

    Upload new website

--- js/script.js
+++
@@ -1,3 +0,0 @@
-const toggleBtn=document.querySelector('.sidebar-toggle');
-const sidebar=document.querySelector('.sidebar');
-if(toggleBtn){toggleBtn.addEventListener('click',()=>{sidebar.classList.toggle('hidden');});}
